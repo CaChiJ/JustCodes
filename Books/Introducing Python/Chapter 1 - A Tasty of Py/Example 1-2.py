c7814a1c64ea8266a6998d10653a0a231c27b6b8
2021037401 <chj7239@hanyang.ac.kr>
2021-04-01 16:11:26 +0900

    Example 1-1, 1-2

---
+++ Books/Introducing Python/Chapter 1 - A Tasty of Py/Example 1-2.py
@@ -0,0 +1,10 @@
+spells = [
+        "Riddikulus!",
+        "Wingardium Leviosa!",
+        "Avada Kedavra!",
+        "Expecto Patronum!",
+        "Nox!",
+        "Lumos!"
+        ]
+
+print(spells[3])
