076bdb5fcdee31c2871503b8167f2b95929c913c
2021037401 <chj7239@hanyang.ac.kr>
2021-04-25 19:52:33 +0900

    no message

---
+++ Books/BrainCSharp5/Chapter2/Problem1/Problem1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Problem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("여러분, 안녕하세요?");
+            Console.WriteLine("반갑습니다.");
+        }
+    }
+}
