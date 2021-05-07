d96b035d8e2c08f663633fcbd5637aa7204ba46f
2021037401 <chj7239@hanyang.ac.kr>
2021-05-07 13:02:38 +0900

    no message

---
+++ Books/BrainCSharp5/Chapter6/Problems/Problems/Problem3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems
+{
+    class Problem3
+    {
+        public static void Plus(int a, int b, out int c)
+        {
+            c = a + b;
+        }
+
+        public static void Plus(double a, double b, out double c)
+        {
+            c = a + b;
+        }
+    }
+}
