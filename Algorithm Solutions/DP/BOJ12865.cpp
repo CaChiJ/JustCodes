41fc1b6b1453686f8ebcfdbe85586d65e9b6a073
2021037401 <chj7239@hanyang.ac.kr>
2021-05-26 11:06:47 +0900

    BOJ12865 optimization

--- Algorithm Solutions/DP/BOJ12865.cpp
+++ Algorithm Solutions/DP/BOJ12865.cpp
@@ -1,53 +1,32 @@
-// DP Knapsack solution
 #include <iostream>
-#include <algorithm>
-#include <cstring>
+#include <vector>
 
 using namespace std;
 
-const int MAXN = 100;
-const int MAXK = 100000;
-int cache[MAXN][MAXK];
-pair<int, int> items[MAXN];
-
-int fillKnapSack(int last, int left)
-{
-	if(last < 0 || left <= 0)
-	{
-		return 0;
-	}
-	
-	if(cache[last][left] == -1)
-	{
-		cache[last][left] = fillKnapSack(last - 1, left);
-		if(left - items[last].first >= 0)
-		{
-			cache[last][left] = max(cache[last][left], fillKnapSack(last - 1, left - items[last].first) + items[last].second);
-		}
-	}
-	
-	return cache[last][left];
-}
-
 int main(void)
 {
-	ios_base::sync_with_stdio(false);
-	memset(cache, -1, sizeof(cache));
-		
-	int N, K;
-	cin>>N>>K;
-	
-	for(int i = 0; i < N; ++i)
-	{
-		cin>>items[i].first>>items[i].second;
-	}
-	
-	int ret = -1;
-	for(int i = 0; i < N; ++i)
-	{
-		ret = max(ret, fillKnapSack(i, K));
-	}
-	
-	cout<<ret<<endl;
-	return 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int N, K;
+    cin >> N >> K;
+    
+    vector<int> weight(N);
+    vector<int> value(N);
+    vector<int> DP(K+1, 0);
+    
+    for(int i = 0; i < N; ++i) {
+        cin >> weight[i] >> value[i];
+    }
+    
+    for(int k = 0; k < N; ++k) {
+        for(int w = K; w >= weight[k]; --w) {
+            DP[w] = max(DP[w], DP[w-weight[k]] + value[k]);
+        }
+    }
+    
+    cout << DP[K] << endl;
+    
+    return 0;
 }
