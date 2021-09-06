63ee371d4744333b36a9f44fef6f21f9c53588de
cachij <chj7239@gmail.com>
2021-09-07 00:47:38 +0900

    BOJ11942 BOJ12727 tutorial solution

---
+++ Algorithm Solutions/Tutorial/BOJ12727.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int dist = scanner.nextInt();
+        int result = dist / 5;
+        if(dist % 5 != 0) {
+            result += 1;
+        }
+        System.out.printf("%d%n", result);
+    }
+}
