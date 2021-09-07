a894bbbaa5d16d886e364039596d51689418da6d
cachij <chj7239@gmail.com>
2021-09-08 02:26:45 +0900

    Segment Tree > Tree

--- Algorithm Solutions/Tree/BOJ2042.cpp
+++ Algorithm Solutions/Tree/BOJ2042.cpp
@@ -1,69 +1,91 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-typedef long long ll;
+vector<long long> prefixSum;
 
-ll numbers[1000000];
-ll seg[1 << 21];
-
-void update(int x, ll v, int l, int r, int I = 1) {
-	if(l == r) {
-		seg[I] = v;
-		return;
-	}
-	
-	int m = (l + r) / 2;
-	
-	if(x <= m) update(x, v, l, m, I * 2);
-	else update(x, v, m+1, r, I * 2 + 1);
-	
-	seg[I] = seg[I * 2] + seg[I * 2 + 1];
-	
-	return;
+long long init(vector<long long>& values, int left, int right, int node) {
+    if(left == right) {
+        return prefixSum[node] = values[left];
+    }
+    
+    int mid = (left + right) / 2;
+    return prefixSum[node] = init(values, left, mid, node * 2) + init(values, mid + 1, right, node * 2 + 1);
 }
 
-
-ll query(int x, int y, int l, int r, int I = 1) {
-	if (y < l || r < x) return 0;
-	if (x <= l && r <= y) return seg[I];
-	int m = (l + r) / 2;
-	return query(x, y, l, m, I * 2) + query(x, y, m + 1, r, I * 2 + 1);
+long long getSum(int left, int right, int node, int nodeLeft, int nodeRight) {
+    if(nodeLeft == left && nodeRight == right) {
+        return prefixSum[node];
+    }
+    
+    int mid = (nodeLeft + nodeRight) / 2;
+    long long result = 0;
+    
+    if(right < mid + 1) {
+        result = getSum(left, right, node * 2, nodeLeft, mid);
+    } else if(left > mid) {
+        result = getSum(left, right, node * 2 + 1, mid+1, nodeRight);
+    } else {
+        result += getSum(left, mid, node * 2, nodeLeft, mid);
+        result += getSum(mid + 1, right, node * 2 + 1, mid + 1, nodeRight);
+    }
+    
+    return result;
 }
 
-
-ll build(int l, int r, int I) {
-	if(l == r) return seg[I] = numbers[l];
-	int m = (l + r) / 2;
-	return seg[I] = build(l, m, I * 2) + build(m + 1, r, I * 2 + 1);
+long long changeValue(int idx, long long value, int node, int left, int right) {
+    if(idx == left && idx == right) {
+        long long diff = value - prefixSum[node];
+        prefixSum[node] = value;
+        return diff;
+    }
+    
+    int mid = (left + right) / 2;
+    long long diff = 0;
+    if(idx <= mid) {
+        diff = changeValue(idx, value, node * 2, left, mid);
+        prefixSum[node] += diff;
+    } else {
+        diff = changeValue(idx, value, node * 2 + 1, mid + 1, right);
+        prefixSum[node] += diff;
+    }
+    
+    return diff;
 }
-
 
 int main(void) {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 	
-	int N, M, K;
-	cin >> N >> M >> K;
-	
-	for(int i = 0; i < N; ++i) {
-		cin >> numbers[i];
-	}
-	
-	build(0, N-1, 1);
-	
-	for(int i = 0; i < M + K; ++i) {
-		int a, b;
-		ll c;
-		cin >> a >> b >> c;
-		
-		if(a == 1) {
-			update(b-1, c, 0, N-1);
-		} else {
-			cout << query(b-1, c-1, 0, N-1) << '\n';
-		}
-	}
-	
-	return 0;
+    int N, M, K;
+    cin >> N >> M >> K;
+    
+    vector<long long> inputs(N+1);
+    
+    for(int i = 1; i <= N; ++i) {
+        cin >> inputs[i];
+    }
+    
+    prefixSum.resize(4 * N + 1);
+    
+    init(inputs, 1, N, 1);
+    
+    for(int i = 0; i < M + K; ++i) {
+    
+        long long a, b, c;
+        cin >> a >> b >> c;
+        
+        switch(a) {
+            case 1:
+                changeValue(b, c, 1, 1, N);
+                break;
+            case 2:
+                cout << getSum(b, c, 1, 1, N) << '\n';
+                break;
+        }
+    }
+    
+    return 0;
 }
